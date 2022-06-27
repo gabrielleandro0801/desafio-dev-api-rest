3b42b3b6faf806cda712e64286e30626aa09da78
Gabriel Leandro <gabriel.sousa@dock.tech>
2022-06-27 17:04:21 -0300

    [develop_docker] Removing volume and adding depends_on

--- api/routes/health_check_route.py
+++ api/routes/health_check_route.py
@@ -6,8 +6,7 @@ from api.controllers.health_check_controller import HealthCheckController
 def add_routes(api: Api) -> Api:
     api.add_resource(
         HealthCheckController,
-        '/',
-        resource_class_kwargs={}
+        '/'
     )
 
     return api
